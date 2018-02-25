c3502ec55b7f465fae7926eb3e136b6db12ff3d0
maxim@otstavnov.ru <maxim@otstavnov.ru>
2018-02-25 17:41:26 +0300

    Changes in the scheme

--- xenon/config.py
+++ xenon/config.py
@@ -3,27 +3,32 @@
 #
 #  config.py
 #
-#               Name        Header      Name
-#               3.3v        1|2         5v
-#               GPIO2 SDA1  3|4         5v
-#               GPIO3 SCL1  5|6         GND
-#               GPIO4       7|8         GPIO14 UART.TXD     <- MOTION_SENSOR_PIN
-#               GND         9|10        GPIO15 UART.RXD
-#               GPIO17      11|12       GPIO18              <- DHT11_SENSOR_PIN
-#               GPIO27      13|14       GND
-#               GPIO22      15|16       GPIO23
-#               3.3v        17|18       GPIO24
-#               GPIO10 MOSI 19|20       GND
-#               GPIO9 MIS0  21|22       GPIO25
-#               GPIO11 SCLK 23|24       GPIO8 CE0
-#               GND         25|26       GPIO7 CE1
-#               SDA0        27|28       SCL0
-#               GPIO5       29|30       GND
-#               GPIO6       31|32       GPIO12
-#               GPIO13      33|34       GND
-#               GPIO19      35|36       GPIO16
-#               GPIO26      37|38       GPIO20
-#               GND         39|40       GPIO21
+#         +-----+-----+---------+---Pi 3---+---------+-----+-----+
+#         | BCM | wPi |   Name  | Physical | Name    | wPi | BCM |
+#         +-----+-----+---------+----++----+---------+-----+-----+
+#         |     |     |    3.3v |  1 || 2  | 5v      |     |     |
+#         |   2 |   8 |   SDA.1 |  3 || 4  | 5v      |     |     |
+#         |   3 |   9 |   SCL.1 |  5 || 6  | 0v      |     |     |
+#         |   4 |   7 | GPIO. 7 |  7 || 8  | TxD     | 15  | 14  | <- (01)
+#         |     |     |      0v |  9 || 10 | RxD     | 16  | 15  |
+#         |  17 |   0 | GPIO. 0 | 11 || 12 | GPIO. 1 | 1   | 18  | <- (02)
+#         |  27 |   2 | GPIO. 2 | 13 || 14 | 0v      |     |     |
+#         |  22 |   3 | GPIO. 3 | 15 || 16 | GPIO. 4 | 4   | 23  |
+#         |     |     |    3.3v | 17 || 18 | GPIO. 5 | 5   | 24  |
+#         |  10 |  12 |    MOSI | 19 || 20 | 0v      |     |     |
+#         |   9 |  13 |    MISO | 21 || 22 | GPIO. 6 | 6   | 25  |
+#         |  11 |  14 |    SCLK | 23 || 24 | CE0     | 10  | 8   |
+#         |     |     |      0v | 25 || 26 | CE1     | 11  | 7   |
+#         |   0 |  30 |   SDA.0 | 27 || 28 | SCL.0   | 31  | 1   |
+#         |   5 |  21 | GPIO.21 | 29 || 30 | 0v      |     |     |
+#         |   6 |  22 | GPIO.22 | 31 || 32 | GPIO.26 | 26  | 12  |
+#         |  13 |  23 | GPIO.23 | 33 || 34 | 0v      |     |     |
+#         |  19 |  24 | GPIO.24 | 35 || 36 | GPIO.27 | 27  | 16  |
+#         |  26 |  25 | GPIO.25 | 37 || 38 | GPIO.28 | 28  | 20  |
+#         |     |     |      0v | 39 || 40 | GPIO.29 | 29  | 21  |
+#         +-----+-----+---------+----++----+---------+-----+-----+
+#         | BCM | wPi |   Name  | Physical | Name    | wPi | BCM |
+#         +-----+-----+---------+---Pi 3---+---------+-----+-----+
 
-MOTION_SENSOR_PIN = 14
-DHT11_SENSOR_PIN = 18
+MOTION_SENSOR_PIN   = 14 # (01)
+DHT11_SENSOR_PIN    = 18 # (02)
